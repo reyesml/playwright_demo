72d0bbe409f185577e44cb11e6f0427a6371d754
reyesml <matthewlreyes+reyesml@gmail.com>
2023-09-13 21:53:03 -0700

    persisting log to file

--- reporters/custom-reporter.ts
+++ reporters/custom-reporter.ts
@@ -1,31 +1,42 @@
 import type {
   Reporter, FullConfig, Suite, TestCase, TestResult, FullResult
 } from '@playwright/test/reporter';
+import * as fs from 'fs';
 
 class CustomReporter implements Reporter {
+
+  private logs = "";
+
   constructor(options: { customOption?: string } = {}) {
     console.log(`initializing CustomReporter. Options: ${options.customOption}`);
   }
 
   onBegin(config: FullConfig, suite: Suite) {
-    log(`Starting the run with ${suite.allTests().length} tests`);
+    this.log(`Starting the run with ${suite.allTests().length} tests`);
   }
 
   onTestBegin(test: TestCase) {
-    log(`Starting test ${test.title}`);
+    this.log(`Starting test ${test.title}`);
   }
 
   onTestEnd(test: TestCase, result: TestResult) {
-    log(`Finished test ${test.title}: ${result.status}`);
+    this.log(`Finished test ${test.title}: ${result.status}`);
   }
 
   onEnd(result: FullResult) {
-    log(`Finished the run: ${result.status}`);
+    this.log(`Finished the run: ${result.status}`);
+    this.persistLogs();
   }
-}
 
-function log(msg: string) : void {
-  console.log(`${timestamp()} | ${msg}`)
+  private log(msg: string) : void {
+    let line=`${timestamp()} | ${msg}`
+    console.log(line)
+    this.logs+=`${line}\n`
+  }
+
+  private persistLogs() : void {
+    fs.writeFileSync('./playwright-report/report.txt', this.logs);
+  }
 }
 
 function timestamp() : string {
